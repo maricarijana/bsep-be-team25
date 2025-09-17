0c3b107a19fa36b8f9b765c250df0c504c3aef5e
Arijana Marić <maricarijana69@gmail.com>
2025-09-17 22:45:25 +0200

    initial project

---
+++ src/main/java/com/example/bsep_team25/BsepTeam25Application.java
@@ -0,0 +1,13 @@
+package com.example.bsep_team25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsepTeam25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsepTeam25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bsep_team25/BsepTeam25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bsep_team25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsepTeam25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
